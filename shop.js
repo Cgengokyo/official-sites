b1da82128f5950ca8b607c7dc2ad700d67e15e7e
Cgengokyoto <109359200+Cgengokyo@users.noreply.github.com>
2023-03-21 01:02:23 +0900

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,5 +1,4 @@
 let radio_btns = document.querySelectorAll(`input[type='radio'][name='type']`);
-
 for (let target of radio_btns) {
   target.addEventListener(`change`, () => {
     document.querySelector(`#output`).innerHTML = `${target.value} : ${target.checked}`;
