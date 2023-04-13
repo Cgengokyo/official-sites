16a8987ba3e6b8589075f11b5c464a0af1e2f370
Cgengokyoto <109359200+Cgengokyo@users.noreply.github.com>
2023-04-13 22:16:05 +0900

    Delete shop.js

--- shop.js
+++
@@ -1,6 +0,0 @@
-let radio_btns = document.querySelectorAll(`input[type='radio'][name='type']`);
-for (let target of radio_btns) {
-  target.addEventListener(`change`, () => {
-    document.querySelector(`#output`).innerHTML = `${target.value} : ${target.checked}`;
-  });
-}
